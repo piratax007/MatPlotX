1f351c6a7b9922d3e46e77635e369574c07b5a85
Fausto M. Lagos S. <piratax007@protonmail.ch>
2025-10-16 22:38:31 +0200

    [F]: Added ruff for linter and code formater, and mypy for type checking

--- tests/test_file_path_exist.py
+++ tests/test_file_path_exist.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+
 from utils.file_utils import file_path_exist
+
 
 def test_file_path_exist_true_for_existing_file(tmp_path: Path) -> None:
     p: Path = tmp_path / "file.txt"
